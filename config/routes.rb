dd754e44d39ea87a97e67410e8a41e961a691b73
jenleebeaver <jen.lee.beaver@gmail.com>
2020-12-28 15:07:05 -0800

    index.html
    testing view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
+  Rails.application.routes.draw do 
     namespace :api do
       namespace :v1 do
         resources :notes, only: [:index, :update]
